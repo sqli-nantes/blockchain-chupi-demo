188963f91e0c5d546f408525bab530ea21a91014
bootnode <a@a.a>
2016-12-15 17:32:57 +0100

    Save beginning sprint 8

--- LaunchBootnode.sh
+++ LaunchBootnode.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo -ne '\033]0;BOOTNODE\007'
 
 echo "Si vous fermez cette fenêtre, Bootnode ne sera pas entièrement fonctionnel."
@@ -17,8 +16,10 @@ cd $NJDir
 
 pwd
 
-node names.js ~/Documents/blockchain-dashboard/src/public/res/json/names.json &
+if [ ! -z "$1" ]; then
+	node names.js ~/Documents/blockchain-dashboard/src/public/res/json/names.json &
+fi
 
 cd $currentDir
 
-sudo docker run -e NETWORKID="100" -v $(pwd)/genesis.json:/tmp/genesis.json -v $NJDir/enode.txt:/home/enode.txt -p 8090:80 -p 30303:30303 -p 8547:8547 --name bootnode blockchain-bootnode
+sudo docker run -e NETWORKID="100" -v $(pwd)/genesis.json:/tmp/genesis.json -v $NJDir/enode.txt:/home/enode.txt -v ~/Documents/blockchain-dashboard/src/public/res/json/names.json:/home/NamesJSON/names.json -p 8081:8081 -p 8090:80 -p 30303:30303 -p 8547:8547 --name bootnode ${1:-blockchain-bootnode}
--- LaunchDashboard.sh
+++ LaunchDashboard.sh
@@ -4,7 +4,7 @@ echo -ne '\033]0;DASHBOARD\007'
 
 sudo service docker start
 
-sudo docker run -d -p 8080:8080 -p 80:80 -p 443:443 -v /home/user/Documents/blockchain-bootnode/NamesJSON/names.json:/home/httpserver/src/public/res/json/names.json --name dashblock dashblock2
+sudo docker run -d -p 8080:8080 -p 80:80 -p 443:443 -v /home/user/Documents/blockchain-bootnode/NamesJSON/names.json:/home/httpserver/src/public/res/json/names.json --name dashblock ${1:-dashblock2}
 
 echo "Press Enter To Quit..."
 
